23f23902dce1eb07ae409c55e90147c002596c58
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-11 05:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function fibonacci(num) {
   if (num === 1) return 0;
   else if (num === 2) return 1;
-  num -= 2;
   var num1 = 0;
   var num2 = 1;
   var sum;
-  var i = 0;
-  for (i = 0; i < num; i++) {
+  for (var i = 2; i < num; i++) {
     sum = num1 + num2;
     num1 = num2;
     num2 = sum;
